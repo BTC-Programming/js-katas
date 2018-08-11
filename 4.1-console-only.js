21cce99e79d1853e469c08532cd8d42a2252e629
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-08-11 18:11:05 -0400

    First Upload

---
+++ 4.1-console-only.js
@@ -0,0 +1,54 @@
+// verbose version
+// initialize variables
+var sentence=[];
+var replacements=[];
+var index=0;
+// initialize users
+alert ("Filler, Go Away.");
+alert ("Enter after each word, end with Shift+6(^)");
+// fill sentence
+populate(index,sentence,"word");
+alert (sentence);
+index=0;
+// specify positions
+populate(index,replacements,"position");
+alert (replacements);
+// enter parts of speech
+index=0;
+replace(index,sentence,replacements,"part of speech");
+alert (sentence);
+// switch people
+// train filler
+alert("Enter words that match the part of speech shown");
+// enter replacement words
+index=0;
+replace(index, sentence, replacements, "new word");
+// show the sentence
+alert("Go get the author to see the sentence!");
+sentenceString = sentence.join(' ');
+alert(sentenceString.toString());
+//end
+
+
+function populate(sindex,sarray,label){
+	do {
+		sarray[sindex]=prompt("Next "+label+"?");
+		sindex = sindex+1;
+	}
+	while (sarray[sindex-1]!="^");
+	console.log(sarray);
+	sarray.splice(-1,1);
+	return sarray;
+}
+
+function replace (sindex,sarray,rarray,label) {
+	for (sindex = 0; sindex < rarray.length; sindex++) {
+		if (label=="new word"){
+			sarray[rarray[sindex]] = prompt("Provide a "+sarray[rarray[sindex]]+" here: ");
+		}
+		else {
+			sarray[rarray[sindex]] = prompt("next "+label+"?");
+		}
+	}
+	return sarray;
+}
