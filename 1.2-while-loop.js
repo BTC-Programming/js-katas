fea41e67c499781e8eda7051c3856e906cf8289d
bmoreinis <bmoreinis@gmail.com>
2018-08-11 13:25:15 -0400

    Instructions Added
    Added Instructions

--- 1.2-while-loop.js
+++ 1.2-while-loop.js
@@ -1,18 +1,24 @@
 /* 1.2 - While Loop */
 
 // Conditional where flavor = chocolate or vanilla
+// Begin with flavor = "start"
+// Accept flavors unless flavor = "done"
 // Prompt for flavor, then output "here's your" + flavor
 // If strawberry, alert "all out of strawberry"
 // Else alert "no such flavor"
-// Continue while flavor not equal to "coffee"
-
-flavor = prompt("What flavor scoop?");
-if (flavor == "chocolate " || flavor == "vanilla"){
-	alert("Here's your"+flavor);
+// If flavor  equal to "done", alert "Enjoy!"
+// Hint: don't use an "else" without a condition in the loop!
+flavor = "start";
+while (flavor != "done") {
+	flavor = prompt("What flavor scoop?");
+	if (flavor == "chocolate" || flavor == "vanilla"){
+	alert("Here's your "+flavor+" scoop.");
+	}
+	else if (flavor == "strawbery") {
+		alert("Sorry, we're all out of "+flavor+".");
+	}
+	else if (flavor != "done"){
+		alert("Sorry, we don't carry "+flavor+".");
+	}
 }
-else if (flavor == "strawbery") {
-	alert("All out of "+flavor);
-}
-else {
-	alert("No such flavor ");
-}
+alert("Enjoy!");
--- README.md
+++ README.md
@@ -9,5 +9,18 @@ Instructions
 		Else alert "Sorry, we don't carry [flavor]"
 	For fun, add other conditionals and responses. 
 
+1.2 - While Loop
+
+Instructions
+
+	Conditional where flavor = chocolate or vanilla
+	Begin with flavor = "start"
+	Use a While Loop to accept flavors unless flavor = "done"
+	Prompt for flavor, then output "here's your" + flavor
+		If strawberry, alert "all out of strawberry"
+		Else alert "no such flavor"
+	If flavor equal to "done", alert "Enjoy!"
+	Hint: don't use an "else" without a condition in the loop!
+
 NOTE: Unlike most GitHub projects, each branch is a SEPARATE SET OF KATAS.  Do not initiate merge requests. 
 
