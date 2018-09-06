f1c1a1d265329d1d3774ec62d590997ac2ba95a2
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-09-06 19:53:17 -0400

    Works (ugly)

--- 21.js
+++ 21.js
@@ -11,7 +11,8 @@ steps=prompt("Starting at "+count+", how many steps do you count (1-3)");
 count=counting(player,count,steps);
 // call counting function for computer, at 2 steps each time
 player="me";
-steps=2; // temporary
+steps=Math.floor(Math.random()*3)+1;
+console.log(steps);
 count=counting(player,count,steps);
 }
 while (count<21);
@@ -22,6 +23,9 @@ function counting(player,current,steps){
 	game=[];
 	for (index=0;index<steps;index++) {
 		game[index]=current+index+1;
+		if (game[index]>20) {
+			game[index]="oops!";
+		}
 	}
 	alert(player+": "+game);
 	current=parseInt(current)+parseInt(steps);
