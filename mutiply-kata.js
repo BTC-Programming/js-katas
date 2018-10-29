3776c9eeb3da3e27160895599f3b9536c5ae8a85
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-10-28 22:00:43 -0400

    Edits

--- mutiply-kata.js
+++ mutiply-kata.js
@@ -14,8 +14,8 @@
  *  GROUP TWO
  *
  */
-// 1. Embed all code in a For loop and run it 10 times.
-// 2. Move code parts 1-3 into a function called multiPractice, and call the function within the For loop.
-// 3. Count how many wrong answers and return count.
-// 4. Calculate wrong answer percent of all answers (hint: all answers = equations + wrong answers)
-// 5. At "stop" or at loop end, report wrong answer percent.  If no wrong answers, say "Charlie, you've won!"
+// 1. Embed all code in a For loop that runs 10 times.
+// 2. Move code parts 1-3 into a function called multiPractice, and call multiPractice within the For loop.
+// 3. Count how many not-right-first-answers and return that count from multiPractice.
+// 4. Accept manual input for loop (not 10 but "questions") and calculate percent correct first time. 
+// 5. At "stop" or at loop end, report answer percent correct.  If no wrong answers, say "Charlie, you've won!"
