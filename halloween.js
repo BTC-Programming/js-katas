d5d8c54c541b8d34ae3734e39263e0c685220ddb
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-10-31 10:40:27 -0400

    Update halloween.js

--- halloween.js
+++ halloween.js
@@ -3,7 +3,5 @@
  */
  
  //1. Display "Trick (1) or Treat (2)?". Display a different message based on choice number with if/elseif. 
- 
- //2. Create function that receives a choice number, returns message based on number with if/elseif.
- 
- //3. Use switch & case rather than if/elseif in function. 
+ //2. Create function that receives a choice number, returns message based on number.
+ //3. Use switch & case rather than if/elseif in function.
