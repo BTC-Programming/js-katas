73565420e598d5a7d440c89d67be51b317b0fa6a
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-10-31 08:48:24 -0400

    7-Adventure

---
+++ halloween.js
@@ -0,0 +1,9 @@
+/* 
+ * Halloween Kata
+ */
+ 
+ //1. Display "Trick (1) or Treat (2)?". Display a different message based on choice number with if/elseif. 
+ 
+ //2. Create function that receives a choice number, returns message based on number with if/elseif.
+ 
+ //3. Use switch & case rather than if/elseif in function. 
