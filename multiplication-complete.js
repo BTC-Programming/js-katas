9ae4f9d05ddb8f43f37428cce149508351384691
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-11-01 20:28:16 -0400

    Fixed errors.  Manual question number input

--- multiplication-complete.js
+++ multiplication-complete.js
@@ -1,33 +1,38 @@
+/* MULTIPLICATION QUIZ KATA */
+
+/* PART 1 */
+
 // 1. Print 2 Random Integers, X & Y, each between 3 and 9, as a soluble equation:  X * Y = ??
 // 2. Receive manual input for solution, say "Correct!" if match, "Incorrect!" if not.
 // 3. Embed equation in While loop that continues until entry matches solution, or = "stop".
 // 4. Embed all code in a For loop and run it 10 times.
 
 /*  initializations */
-var x = 0;
-var y = 0;
-var solution = 0; // calculated value
-var userInput = 0; // manual input value
-var stop = false;  // signal value
-var wrongAnswers=0; // count wrong answers
-var questions=10; // set number of questions
-var wrong = 0; // within function, count number of bad tries
-
+var x = 0; // x value
+var y = 0; // y value
+var solution = 0; // solution: calculated value
+var userInput = 0; // userInput: manual input value
+var stop = false;  // stop: signal value
+var wrongAnswers=0; // wrongAnswer: count wrong answers
+var questions=10; // questions: set number of questions
+var wrong = 0; // wrong: within function, count number of bad tries
+questions = prompt("How many questions?");
 /* code */
-// 4. Embed all code in a For loop and run it 10 times.
-for (question = 1; question <=questions; question++) {
+// 4. Embed all code in a For loop, start at 0, end at number of questions.
+for (question = 0; question < questions; question++) {
   // run function and return number of wrong answers, add them to previous count
-   wrongAnswers+=multiPractice(question);
+   wrongAnswers+=multiPractice();
 } // end Loop
-// Display score
-if (wrongAnswers<0) {
-alert("You had "+wrongAnswers+" wrong answers out of "+questions+ "questions.");
+// Display score as wrong out of questions if any wrong answers
+if (wrongAnswers>0) {
+	alert("You had "+wrongAnswers+" wrong answers out of "+questions+ " questions.");
 }
+// Display "Charlie, you've won!" if no wrong answers
 else alert ("Charlie, you've won!!");
 
   
 // FUNCTIONS
-function multiPractice(question){
+function multiPractice(){
    wrong=0; // re-set each time function is called
    // 1. Print 2 Random Integers, X & Y, each between 3 and 9, as a soluble equation:  X * Y = ??
    x = Math.floor(Math.random()*7)+3;
@@ -36,19 +41,30 @@ function multiPractice(question){
    // 3. Embed equation in While loop that continues until entry matches solution, or = "stop".
    while ((userInput != solution) && (stop==false)) {
    // 2. Receive manual input for solution, say "Correct!" if match, "Incorrect!" if not.
-      userInput = prompt("Question "+question+": What is "+x+" * "+y+"?");
+      userInput = prompt("What is "+x+" * "+y+"?");
+	   // check if userInput = stop
       if (userInput == "stop" || userInput == "Stop") {
+		 // change signal variable if stop
          stop = true;
-      }  // end if 
+      // end if 
+	  } 
+	  // check if userInput = solution
       else if (userInput == solution) {
+		// say Correct! 
          alert("Correct!");
-      } // end else if
-      else {
+		  // end else if
+      } 
+	  // check if userInput not equal to solution 
+      else if (userInput != solution) {
+		 // say Incorrect, Try Again
          alert("Incorrect solution. Try again.");
+		 // count a wrong answer
          wrong++;
       } // end Else
    } // end While
-   return wrong;
+	// set wrong = 1 if wrong is greater than 0
+	if (wrong > 0) wrong = 1;
+	console.log("wrong: "+wrong);
+	// return wrong
+   return wrong; 
 }
-
-
