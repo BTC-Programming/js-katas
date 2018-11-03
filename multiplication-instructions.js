a3bd46c5ef781b50bdd29743d3450c92db3bc18c
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-11-03 11:46:01 -0400

    Update multiplication-instructions.js

--- multiplication-instructions.js
+++ multiplication-instructions.js
@@ -6,11 +6,11 @@
 // 1. Print 2 Random Integers, X & Y, each between 3 and 9, as a soluble equation:  X * Y = ??
 // 2. Receive manual input for solution, say "Correct!" if match, "Incorrect!" if not.
 // 3. Embed equation in While loop that continues until entry matches solution, or = "stop".
-// 4. Embed all code in a For loop and run it 10 times.
+// 4. Get Manual Input for "totalQuestions" (e.g. 10).
+// 5. Embed all code in a For loop and run it totalQuestions times.
 
-/* INSTRUCTIONS for PART 2: REFACTOR INTO FUNCTIONS */
+/* REFACTOR INTO FUNCTION */
 
-// 5. Move code parts 1-3 into a function called multiPractice, and call multiPractice within the For loop.
-// 6. Count how many not-right-first-answers and return that count from multiPractice.
-// 7. At "stop" or loop end, report # answers wrong.  If no wrong answers, say "Charlie, you've won!"
-// 8. Extra Credit: instead of 10 times, run loop to value of questions variable entered manually. 
+// 6. Move code parts 1-3 into a function called multiPractice, and call multiPractice within the For loop.
+// 7. If a wrong answer was submitted on a question, return 1 from multiPractice and add to variable called wrongAnswers.
+// 8. At "stop" or loop end, report # answers wrong.  If wrongAnswers = 0, say "Charlie, you've won!"
