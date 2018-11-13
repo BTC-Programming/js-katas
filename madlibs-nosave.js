2be241bea234b6e7b5eaedabb7625d0d3bfee321
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-11-12 19:29:36 -0500

    Add files via upload

---
+++ madlibs-nosave.js
@@ -0,0 +1,35 @@
+// Code for madlibs.js
+document.getElementById("startGame").addEventListener("click", startGame);
+
+function startGame() {
+
+	// initialize variables
+	var sentenceString=null; // initial sentence
+	var sentence = [];
+	var whichWords = [];
+	var replacements = 0;
+	var removeMarkup = null;
+
+	// Input the sentence, save as array(author)
+	sentenceString = prompt("Author, enter a sentence.");
+	sentence=sentenceString.split(" ");
+	alert("OK, "+arraySentence(sentence));
+
+	// Specify the replacements positions and parts of speech (author)
+	replacements = prompt("Replace how many words?");
+	for (word=0; word<replacements;word++) {
+		whichWords[word] = (prompt("Which words? Word position: ")-1);
+		sentence[whichWords[word]]=prompt("Part of speech of "+sentence[whichWords[word]]+"?");
+	}
+
+	alert("Author, you're done. Player, your turn!");
+	for (word=0; word<replacements;word++) {
+		sentence[whichWords[word]] = prompt("Enter a "+sentence[whichWords[word]]+": ");
+	}
+	alert(arraySentence(sentence));
+}
+//Convert array to sentence
+function arraySentence(myArray) {
+	removeMarkup = myArray.join(" ");
+	return removeMarkup;
+}
