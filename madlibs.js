32a38f91e499f7ae216b41cce9e436d60c8ff04c
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-11-12 19:22:44 -0500

    Using split to convert string to array.

--- madlibs.js
+++ madlibs.js
@@ -4,70 +4,49 @@ document.getElementById("startGame").addEventListener("click", startGame);
 function startGame() {
 
 	// initialize variables
+	var sentenceString=null; // initial sentence
 	var sentence = [];
-	var replacements = [];
-	var removeMarkup = "";
+	var whichWords = [];
+	var replacements = 0;
+	var removeMarkup = null;
 
-	// Input the sentence (author)
-	alert("Author, enter the original sentence word by word. End with \"^\".");
-	sentence=populate(sentence, "Word "); // put words into the sentence array
-	oldSentence = sentence.pop();
-	oldSentence = arraySentence(sentence); // Output the sentence without the ^ or commas
+	// Input the sentence, save as array(author)
+	sentenceString = prompt("Author, enter a sentence.");
+	sentence=sentenceString.split(" ");
+	alert("OK, "+arraySentence(sentence));
 
-	// Specify the replacements positions (author)
-	replacements=swapulate(replacements,sentence.length,"Enter positions to replace, first is 0. End with \"^\".");
+	// Specify the replacements positions and parts of speech (author)
+	replacements = prompt("Replace how many words?");
+	for (word=0; word<replacements;word++) {
+		whichWords[word] = (prompt("Which words? Word position: ")-1);
+		sentence[whichWords[word]]=prompt("Part of speech of "+sentence[whichWords[word]]+"?");
+	}
 
-	// Specify parts of speech for replacements (author)
-	sentence=swap(sentence,replacements,"Enter Part of Speech for each word: ");
+	alert("Author, you're done. Player, your turn!");
+	for (word=0; word<replacements;word++) {
+		sentence[whichWords[word]] = prompt("Enter a "+sentence[whichWords[word]]+": ");
+	}
 
-	// Enter replacement words (player)
-	alert("Author, you're done. Player, your turn!")
-	sentence=swap(sentence,replacements,"Enter a "); // enter replaced words
-	newSentence=arraySentence(sentence);  // Output the sentence without the ^ or commas
+	alert(arraySentence(sentence));
 
 	// Save sentence to file and end game
-	var gameText = "Old: "+oldSentence+"\nNew: "+newSentence;
+	var gameText = "Old: "+sentenceString+"\nNew: "+arraySentence(sentence);
 	alert(gameText);
 	saveText(gameText);
-	};
+	}
 
 //end game
 
 // Build Sentence Array
-function populate(myArray,sprompt){
-	index=0;
-	do {
-	myArray[index] = prompt(sprompt + index);
-	index++;
-	}
-	while(myArray[index - 1] != "^")
-	return myArray;
-}
 
-function swapulate(myArray,length,sprompt){
-	index=0;
-	do {
-	myArray[index] = prompt(sprompt);
-	index++;
-	}
-	while(myArray[index - 1] != "^");
-	return myArray;
-}
 
 
 // Convert array to sentence function
 function arraySentence(myArray) {
-	removeMarkup = myArray.join(' ')+".";
+	removeMarkup = myArray.join(" ");
 	return removeMarkup;
 }
 
-// Swap itemes in Sentence Array for Parts of Speech, New Words
-function swap(sarray,rarray,sprompt,index){
-	for (index = 0; index < rarray.length - 1; index++){
-		sarray[rarray[index]]= prompt(sprompt + sarray[rarray[index]]);
-	}
-	return sarray;
-}
 
 function saveText(text) {
 	alert("Clicking OK will save this game in downloads folder as madlib.txt");
